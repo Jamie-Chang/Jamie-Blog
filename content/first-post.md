77c123d5cc0552f113cca721c4e921063a5eefce
Jamie Chang <jamie.chang@Jamies-MBP.cust.communityfibre.co.uk>
2024-09-18 23:05:21 +0100

    Use pelican static site generator

---
+++ content/first-post.md
@@ -0,0 +1,8 @@
+Title: My First Blog Post
+Date: 2024-09-18
+Category: Blog
+
+My name is Jamie Chang, I'm a software engineer in London. I like all things Python, but I'll try to mix it up sometimes. I decided to start this blog to get some of the ideas out from my head and to practice communicating technical topics.
+
+
+For this site, I'm starting off with a Python static site generator called [Pelican](https://docs.getpelican.com/) and Github pages to host the site.
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Jamie Chang'
+SITENAME = "Jamie's Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+    ("Github", "https://github.com/Jamie-Chang/"),
+    ("LinkedIn", "https://www.linkedin.com/in/jamie-chang-4423ba125/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
